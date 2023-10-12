1454e07e118bf3a82c0b7fc0cf06eb5fccd785d8
Anthony <fuzionzeak@gmail.com>
2023-10-12 10:38:14 +0200

    Premier commit

---
+++ ClassesFlotte/Tourisme.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesFlotte
+{
+    public class Tourisme : Vehicule
+    {
+        private int nbPlaces;       // nombre de places du vehicule de tourisme
+        private Modele leModele;    // modèle du véhicule de tourisme (object Modele)
+
+        /// <summary>
+        /// Constructeur de la classe Tourisme
+        /// </summary>
+        public Tourisme(string uneImmat, string uneEnergie, int unePuissance, int unNombrePlaces, Modele unModele)
+            : base(uneImmat, uneEnergie, unePuissance)
+        {
+            this.nbPlaces = unNombrePlaces;
+            this.leModele = unModele;
+        }
+
+        /// <summary>
+        /// Retourne un booléen indiquant si le vehicule de tourisme est à entretenir ou non
+        /// un véhicule de tourisme doit être révisé tous les 2 ans 
+        /// ou selon la fréquence kilométrique fonction du modèle de véhicule
+        /// </summary>
+        /// <returns>true si un entretien est à prévoir ou false dans le cas contraire</returns>
+        public override bool PrevoirEntretien()
+        {
+            bool prevoirEnt = false;
+            DateTime dateDernEnt = base.GetDernierEntretien().GetDateEntretien();
+            DateTime dateActuelle = DateTime.Now;
+            TimeSpan diffDates = dateActuelle.Subtract(dateDernEnt);
+
+            if (diffDates.Days >= 720)
+                prevoirEnt = true;
+            return prevoirEnt;
+        }
+    }
+}
---
+++ ClassesFlotteTests/UtilitaireTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassesFlotte;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesFlotte.Tests
+{
+    [TestClass()]
+    public class UtilitaireTests
+    {
+        [TestMethod()]
+        public void ChargeUtileTest()
+        {
+            // TODO
+        }
+
+        [TestMethod()]
+        public void PrevoirEntretienTest()
+        {
+            // TODO
+        }
+    }
+}
